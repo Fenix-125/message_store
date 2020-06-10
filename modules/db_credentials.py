d3f4b96cea0f59ac42a37e9c9a483ac0b798adef
anastasiia <tananaiska@ucu.edu.ua>
2020-06-10 08:18:33 +0300

    Added valid db credentials

--- modules/db_credentials.py
+++ modules/db_credentials.py
@@ -8,5 +8,6 @@ class DBInfo:
         self.paswd: str = paswd
 
 
+
 db_auth = DBInfo("ec2-3-222-30-53.compute-1.amazonaws.com", "5432", "d9i05h7i78ht63",
                  "ivipfsifsguaeg", "44df42a96145dbb51d560637209323426da5dab23e50b5fb6244de8bb8bd595a")
